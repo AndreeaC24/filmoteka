a44fdbe9660d1b63337c7d2be188875883ebb72a
AndreeaC24 <119935685+AndreeaC24@users.noreply.github.com>
2023-06-16 21:41:51 +0000

    Deploying to gh-pages from @ AndreeaC24/filmoteka@2ea4e21ab825d5d19cd434967a4cec2c1d52596d 🚀

---
+++ index.2d30d8cc.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("html");["ae","ar","arc","bcc","bqi","ckb","dv","fa","glk","he","ku","mzn","nqo","pnb","ps","ro","sd","ug","ur","yi"].includes(e.getAttribute("lang"))||e.style.removeProperty("padding-left")}();
+//# sourceMappingURL=index.2d30d8cc.js.map
